813ae69a11fb0b7eec4d5c38e6273a06a17b64fe
Andrei <andrei.zacordonet@student.tuiasi.ro>
2024-04-17 17:35:41 +0300

    indexedDB trial

--- continut/js/cumparaturi.js
+++ continut/js/cumparaturi.js
@@ -1,4 +1,5 @@
-// Define a class for Product
+//----------------------------------------------------------------------------
+// product class
 class Product {
     static id = 0;
     constructor(nume, cantitate) {
@@ -7,7 +8,10 @@ class Product {
         this.cantitate = cantitate;
     }
 }
+//----------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------
+// storage base class
 class Storage {
     constructor() {
     }
@@ -24,13 +28,15 @@ class Storage {
     // getAllItems() {
     // }
 }
+//----------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------
+// local storage class
 class LocalStorage extends Storage{
     constructor(){
         super();
     }
 }
-
 LocalStorage.prototype.add = function saveProduct(product) {
     // Retrieve existing products from localStorage or initialize an empty array
     let products = JSON.parse(localStorage.getItem('products')) || [];
@@ -41,6 +47,19 @@ LocalStorage.prototype.add = function saveProduct(product) {
     // Save the updated array back to localStorage
     localStorage.setItem('products', JSON.stringify(products));
 };
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// indexedDB class
+class indexDB extends Storage{
+    constructor(){
+        super();
+    }
+}
+indexDB.prototype.add = function(product){
+    console.log('ciorba de vacuta indexata DBDBDBDBBD');
+};
+
 
 function shopping(){
     const worker = new Worker('js/worker.js');
@@ -96,6 +115,75 @@ function shopping(){
         locStor.add(product);
 
         // saveProduct(product);
+    });
+
+    let btnV2 = document.getElementById('adaugaBtnV2');
+    btnV2.addEventListener('click', function (event){
+
+        let db;
+        const request = window.indexedDB.open("MyTestDatabase", 3);
+
+        request.onerror = (event) => {
+            // Do something with request.errorCode!
+            console.error("Why didn't you allow my web app to use IndexedDB?!");
+        };
+        request.onsuccess = (event) => {
+            // Do something with request.result!
+            db = event.target.result;
+            // const data = event.data;
+            // console.log('Received message from worker:', data);
+            // console.log('Received message type:', typeof (data));
+
+            // // Add a new row to the table with the data received from the worker
+            // let table = document.getElementById('shoppingTable');
+            // const row = table.insertRow(-1);
+            // const td1 = document.createElement('td');
+            // console.log(data.nume);
+            // td1.textContent = Product.id;
+            // row.appendChild(td1);
+
+            // const td2 = document.createElement('td');
+            // td2.textContent = data.nume;
+            // row.appendChild(td2);
+
+            // const td3 = document.createElement('td');
+            // td3.textContent = data.cantitate;
+            // row.appendChild(td3);
+        };
+        db.onerror = (event) => {
+            // Generic error handler for all errors targeted at this database's
+            // requests!
+            console.error(`Database error: ${event.target.errorCode}`);
+        };
+
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+
+            // Create an objectStore to hold information about our customers. We're
+            // going to use "ssn" as our key path because it's guaranteed to be
+            // unique - or at least that's what I was told during the kickoff meeting.
+            const objectStore = db.createObjectStore("produse", { keyPath: "id" });
+
+            // Create an index to search customers by name. We may have duplicates
+            // so we can't use a unique index.
+            objectStore.createIndex("nume", "nume", { unique: false });
+
+            // Create an index to search customers by email. We want to ensure that
+            // no two customers have the same email, so use a unique index.
+            objectStore.createIndex("cantitate", "cantitate", { unique: false });
+
+            // Use transaction oncomplete to make sure the objectStore creation is
+            // finished before adding data into it.
+            objectStore.transaction.oncomplete = (event) => {
+                // Store values in the newly created objectStore.
+                const produseObjectStore = db
+                    .transaction("produse", "readwrite")
+                    .objectStore("produse");
+                produseData.forEach((produs) => {
+                    produseObjectStore.add(produs);
+                });
+            };
+        };
     });
 }
 
